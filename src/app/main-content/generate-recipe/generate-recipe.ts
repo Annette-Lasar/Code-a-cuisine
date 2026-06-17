9672b233c2adcffef5cc45281a5b2008fce58977
Annette Lasar <146415092+Annette-Lasar@users.noreply.github.com>
2026-06-17 08:17:02 +0200

    Implement basic code for ingredients form

--- src/app/main-content/generate-recipe/generate-recipe.ts
+++ src/app/main-content/generate-recipe/generate-recipe.ts
@@ -1,16 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 import { ThemeService } from '../../shared/services/theme.service';
+
 import { Logo } from '../../shared/components/logo/logo';
 
 @Component({
   selector: 'cuis-generate-recipe',
-  imports: [RouterModule, Logo],
+  imports: [RouterModule, ReactiveFormsModule, Logo],
   templateUrl: './generate-recipe.html',
   styleUrl: './generate-recipe.scss',
 })
 export class GenerateRecipe {
   private themeService = inject(ThemeService);
+  private fb = inject(FormBuilder);
+
+  ingredientForm = this.fb.group({
+    ingredient: [''],
+    amount: [''],
+    unit: ['gram'],
+  });
+
+  submitForm() {
+    console.log(this.ingredientForm.value);
+    
+  }
 
   getThemeClass(): string {
     return this.themeService.currentTheme();
